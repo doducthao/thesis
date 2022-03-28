3e17f726d6380dd6bf0b9da0eac636e40921c189
Do Duc Thao <47765478+doducthao@users.noreply.github.com>
2022-03-28 16:37:23 +0700

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,122 @@
+import sys
+import numpy as np
+import imageio
+
+def save_images(images, size, image_path):
+    return imsave(images, size, image_path)
+
+def imsave(images, size, path):
+    image = np.squeeze(merge(images, size))
+    return imageio.imwrite(path, image)
+
+def merge(images, size):
+    h, w = images.shape[1], images.shape[2]
+    if (images.shape[3] in (3,4)):
+        c = images.shape[3]
+        img = np.zeros((h * size[0], w * size[1], c))
+        for idx, image in enumerate(images):
+            i = idx % size[1]
+            j = idx // size[1]
+            img[j * h:j * h + h, i * w:i * w + w, :] = image
+        return img
+    elif images.shape[3]==1:
+        img = np.zeros((h * size[0], w * size[1]))
+        for idx, image in enumerate(images):
+            i = idx % size[1]
+            j = idx // size[1]
+            img[j * h:j * h + h, i * w:i * w + w] = image[:,:,0]
+        return img
+    else:
+        raise ValueError('in merge(images,size) images parameter ''must have dimensions: HxW or HxWx3 or HxWx4')
+
+
+
+
+def parameters_string(module):
+    lines = [
+        "",
+        "List of model parameters:",
+        "=========================",
+    ]
+
+    row_format = "{name:<40} {shape:>20} ={total_size:>12,d}"
+    params = list(module.named_parameters())
+    for name, param in params:
+        lines.append(row_format.format(
+            name=name,
+            shape=" * ".join(str(p) for p in param.size()),
+            total_size=param.numel()
+        ))
+    lines.append("=" * 75)
+    lines.append(row_format.format(
+        name="all parameters",
+        shape="sum of above",
+        total_size=sum(int(param.numel()) for name, param in params)
+    ))
+    lines.append("")
+    return "\n".join(lines)
+
+
+def assert_exactly_one(lst):
+    assert sum(int(bool(el)) for el in lst) == 1, ", ".join(str(el) for el in lst)
+
+
+class AverageMeterSet:
+    def __init__(self):
+        self.meters = {}
+
+    def __getitem__(self, key):
+        return self.meters[key]
+
+    def update(self, name, value, n=1):
+        if not name in self.meters:
+            self.meters[name] = AverageMeter()
+        self.meters[name].update(value, n)
+
+    def reset(self):
+        for meter in self.meters.values():
+            meter.reset()
+
+    def values(self, postfix=''):
+        return {name + postfix: meter.val for name, meter in self.meters.items()}
+
+    def averages(self, postfix='/avg'):
+        return {name + postfix: meter.avg for name, meter in self.meters.items()}
+
+    def sums(self, postfix='/sum'):
+        return {name + postfix: meter.sum for name, meter in self.meters.items()}
+
+    def counts(self, postfix='/count'):
+        return {name + postfix: meter.count for name, meter in self.meters.items()}
+
+
+class AverageMeter:
+    """Computes and stores the average and current value"""
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+def export(fn):
+    mod = sys.modules[fn.__module__]
+    if hasattr(mod, '__all__'): 
+        mod.__all__.append(fn.__name__)
+    else:
+        mod.__all__ = [fn.__name__]
+    return fn
+
+
+def parameter_count(module):
+    return sum(int(param.numel()) for param in module.parameters())
